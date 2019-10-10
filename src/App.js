5ebcc2a469ff9359534d11db9fd83ab94ab3001e
Christopher  Tso <christophertso@Christophers-iMac.local>
2019-10-09 22:20:34 -0500

    Added todoitem component and add todo capability

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState('')
+  const [id, setId] = useState(0)
+
+  const handleSubmit = () => {
+    setTodos([...todos, {'text': todo, 'id': id}]); 
+    setTodo('');
+    setId(id+1);
+  }
   return (
     <div>
       Count: {count}
       <button onClick={() => {setCount(count+1)}} value = 'Increment'>Increment</button>
+      <div>
+        <input type = 'text' onChange={event => setTodo(event.target.value)}></input>
+        <input type = 'submit' onClick={handleSubmit}></input>
+        {todos.length > 0 ? todos.map(todo => <ToDoItem todo={todo}/>) : null}
+      </div>
     </div>
   );
+}
+
+function ToDoItem({todo}){
+  return (
+    <div>
+      {todo.text}
+    </div>
+  )
 }
 
 export default App;
